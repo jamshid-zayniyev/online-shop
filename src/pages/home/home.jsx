085f9e1a5e914a6fd8d20f9f18bc3dfa51e07426
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2024-12-14 15:32:22 +0500

    home section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+// image
+import home from "../../assets/home.jpg"
+
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className='relative'>
+        <img className='absolute w-full h-[100vh] object-cover' src={home} alt="home" />
+        <div className=' container absolute flex flex-col text-white justify-center items-start w-full min-h-[100vh]'>
+          <div className='backdrop-blur-md  bg-opacity-40 w-full rounded-[15px] p-2'>
+          <h1 className='text-[26px] md:text-[50px] font-bold '>NEW SEASON ARRIVALS</h1>
+          <p className='text-[20px] md:text-[25px] '>CHECK OUT ALL TRENDS</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
